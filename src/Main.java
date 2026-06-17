51b90813e571f01fb6a686840e0367c2fa52a6ac
Peixoto2007 <dyegolucas2020@gmail.com>
2026-06-16 21:01:48 -0300

    Primeiros códigos em Java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+        System.out.print(" Eae ");
+        int idade = 8 ;
+        System.out.print(idade);
+        double velocidade;
+        double total = 15d/6;
+        System.out.println(total);
+
+    }
+}
