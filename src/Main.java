0520f0b8f4e45de84a65d2825d83f242695265ba
Peixoto2007 <dyegolucas2020@gmail.com>
2026-06-23 21:55:16 -0300

    Primeiros codigos em Java ( estava esquecendo de salvar no github )

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World !");
-        System.out.print(" Eae ");
-        int idade = 8 ;
-        System.out.print(idade);
-        double velocidade;
-        double total = 15d/6;
-        System.out.println(total);
 
-    }
-}
+
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+                System.out.println("Digite seu peso e sua altura");
+                Scanner teclado = new Scanner(System.in);
+                int peso = teclado.nextInt();
+                double altura = teclado.nextDouble();
+                double imc = peso /(altura*altura);
+                System.out.println(imc);
+
+                String msg;
+                Scanner teclado1 = new Scanner (System.in);
+                System.out.println("Digite seu palpite : ");
+                int palpite = teclado1.nextInt();
+                int valorcorreto = 10000;
+
+                while (palpite != valorcorreto){
+                        msg = palpite > valorcorreto ? "Um pouco menos..." : "Um pouco mais...";
+                        System.out.println(msg);
+
+                        System.out.print("Tente de novo: ");
+                        palpite = teclado.nextInt();
+                }
+                        /*if ( palpite > valorcorreto){
+                                System.out.println("UM pouco menos");
+                                System.out.println("Digite outro palpite : ");
+                                palpite = teclado.nextInt();
+                        }
+                        else{
+                                System.out.println("Um pouco mais");
+                                System.out.println("Digite outro palpite : ");
+                                palpite = teclado.nextInt();
+                        }*/
+                        System.out.println("End");
+                }
+
+        }
+
+
+
